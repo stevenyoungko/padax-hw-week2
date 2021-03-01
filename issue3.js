568b0cc6a39e0acdcb39840133ddb162af949690
steven <stevenke60815@gmail.com>
2021-03-01 19:59:54 +0800

    [fix] issue3

--- issue3.js
+++ issue3.js
@@ -1,5 +1,5 @@
 function maxProduct(nums){
-	let max = 0
+	let max = nums[0] * nums[1]
 	for(let i = 0; i < nums.length - 1; i++) {
 		for (let j = 0; j < nums.length-i-1; j++ ) {
 			temp = nums[i] * nums[i+j+1]
@@ -14,3 +14,4 @@ function maxProduct(nums){
 
 console.log(maxProduct([5, 20, 2, 6]))
 console.log(maxProduct([10, -20, 0, 3]))
+console.log(maxProduct([1, -10]))
--- issue3.py
+++ issue3.py
@@ -1,11 +1,13 @@
 def maxProduct(nums):
-	max = 0
+	max = nums[0] * nums[1]
 	for i in range(len(nums)-1):
 		for j in range(len(nums)-i-1):
 			temp = nums[i] * nums[i+j+1]
 			if (temp > max):
 				max = temp
+
 	return max
 
 print(maxProduct([10, -20, 0, 3]))
 print(maxProduct([5, 20, 2, 6]))
+print(maxProduct([1, -10]))
